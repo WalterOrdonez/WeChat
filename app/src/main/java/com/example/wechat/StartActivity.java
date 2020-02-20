9ef000b1407e82e668a151f6385c24ec697aaa09
Walter Ordoñez <walter.ordonez.garcia@gmail.com>
2020-02-20 08:17:56 -0600

    Activities Login, Start

--- app/src/main/java/com/example/wechat/StartActivity.java
+++ app/src/main/java/com/example/wechat/StartActivity.java
@@ -1,14 +1,36 @@
 package com.example.wechat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
+
+    AppCompatButton login, register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=findViewById(R.id.login);
+        register=findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class));
+            }
+        });
     }
 }
